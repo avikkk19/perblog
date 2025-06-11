a2b5f7f1f0b2a9d2824600747ac8673fc069bc09
avi <164750977+avikkk19@users.noreply.github.com>
2025-06-11 22:53:00 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter future={{ v7_relativeSplatPath: true }}>
     <React.StrictMode>
       <App />
+      <Analytics />
     </React.StrictMode>
   </BrowserRouter>
 );
